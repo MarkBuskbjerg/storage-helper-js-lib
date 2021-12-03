1356782018d2385e28b0a4df50f2761636f9c8cc
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2021-12-03 18:37:09 +0100

    added a method to get item from the browsers localStorage

--- storage-helper.js
+++ storage-helper.js
@@ -15,13 +15,21 @@ let storageHelper = (function () {
   }
 
   /**
-   * Set a browser storage item // Save data to the browser storage
+   * Set a browser storage item // Save data to the browsers localStorage
    * Uses the localStorage.setItem() method - all values are stringified to make the data easier to parse for other devs using our localStorage data in the future
    * @param {*} value The storage value
    */
   Constructor.prototype.set = function (value) {
     localStorage.setItem(this._key, JSON.stringify(value));
-  }
+  };
+
+  /**
+   * Get a browser storage item // Get data from the browsers localStorage
+   * @return {*} The storage value
+   */
+  Constructor.prototype.get = function () {
+    return JSON.parse(localStorage.getItem(this._key));
+  };
 
   return Constructor
 })();
