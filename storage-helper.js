626660a0bc8f1ca828b3d535ea2233ea32e3065f
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2021-12-03 18:30:52 +0100

    added an object parameter for options

--- storage-helper.js
+++ storage-helper.js
@@ -2,11 +2,15 @@ let storageHelper = (function () {
 
   /**
    * The Constructor Object
-   * @param {String} key  The storage key 
+   * @param {String} key  The storage key
+   * @param {Object} options User options and settings
    */
 
-  function Constructor(key) {
+  function Constructor(key, options = {}) {
     // This is where we will set our properties for the library ...
+    Object.defineProperties(this, {
+      _key: { value: key }
+    });
   }
 
   return Constructor
