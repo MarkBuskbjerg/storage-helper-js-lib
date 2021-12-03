d97854a90d12b4b548656185c11396a2acbd67e2
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2021-12-03 18:27:33 +0100

    Established the shell with a Constructor Pattern

--- storage-helper.js
+++ storage-helper.js
@@ -1,1 +1,13 @@
-console.log('storage-helper is running');
+let storageHelper = (function () {
+
+  /**
+   * The Constructor Object
+   * @param {String} key  The storage key 
+   */
+
+  function Constructor(key) {
+    // This is where we will set our properties for the library ...
+  }
+
+  return Constructor
+})();
