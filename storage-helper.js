49942018c22b8b9c8fb28c7332d61403f2b02aa3
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2021-12-03 19:23:56 +0100

    Added emitEvent function to make it easy to assign custom events that we can hook into when using the library

--- storage-helper.js
+++ storage-helper.js
@@ -1,5 +1,26 @@
 let storageHelper = (function () {
 
+  /**
+   * Emit a custom event
+   * @param {String}  type    The event type
+   * @param {Object}  detail  Any details to pass along with the event
+   * @param {Node}    elem    The element to attach the event to
+   */
+  function emitEvent(type, detail = {}, elem = document) {
+    // Make sure there's an event type
+    if (!type) return;
+
+    // Create a new event
+    let event = new CustomEvent(type, {
+      bubbles: true,
+      cancelable: true,
+      detail: detail
+    });
+
+    // Dispatch the event
+    return elem.dispatchEvent(event);
+  }
+
   // Default settings
   let defaults = {
     type = 'localStorage'
