b19de8945e92cd586c13894e9e56d31df5470c64
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2021-12-03 18:34:31 +0100

    Added method to save data to localStorage

--- storage-helper.js
+++ storage-helper.js
@@ -11,6 +11,16 @@ let storageHelper = (function () {
     Object.defineProperties(this, {
       _key: { value: key }
     });
+
+  }
+
+  /**
+   * Set a browser storage item // Save data to the browser storage
+   * Uses the localStorage.setItem() method - all values are stringified to make the data easier to parse for other devs using our localStorage data in the future
+   * @param {*} value The storage value
+   */
+  Constructor.prototype.set = function (value) {
+    localStorage.setItem(this._key, JSON.stringify(value));
   }
 
   return Constructor
